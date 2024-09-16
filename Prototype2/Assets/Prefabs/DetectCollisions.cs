d0bc595cf5691a963f27c3c2fd94ea374708c915
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-16 00:32:40 -0500

    Update Detect Collision Script

---
+++ Prototype2/Assets/Prefabs/DetectCollisions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private DisplayScore displayerScoreScript; 
+    
+    private void Start()
+    {
+        displayerScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<displayerScoreScript>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        displayerScoreScript.score++;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
